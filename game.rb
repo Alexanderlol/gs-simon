83d0869e0fdf5b73993c16910d5c6be692f3513e
Alex Afeltra <afeltra.alex@gmail.com>
2017-05-01 20:52:17 -0400

    AMAZING GAME 10/10 ON STEAM RAVING REVIEWS

--- game.rb
+++ game.rb
@@ -1,5 +1,15 @@
-require 'green_shoes'
-
-Shoes.app(title: "gameBoard", width: 240, height: 240) do
-
+def greeting
+	puts "Welcome to Simon! Please enter your name: "
+	name = gets.chomp
+	puts "Welcome to the game, #{name}!"
+	puts "Please wait while i fuck ur mother and generate a random pattern!"
+	sleep(3)
 end
+
+def correctPattern
+	Array.new(4) { rand(1..4) }
+end
+
+puts greeting
+puts correctPattern
+
